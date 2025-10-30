f9f96fa2712b46180484c3129eb4ee38db927d5c
FLOYD <sungmin.park5941@gmail.com>
2025-10-30 15:28:42 +0900

    Spring Core 빈 후처리기

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,13 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicprocxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicprocxy.DynamicProxyFilterConfig;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -21,7 +15,9 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 //컴포넌트 스캔의 대상 제한을 위해 app으로 설정
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
